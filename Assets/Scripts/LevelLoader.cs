d8a330a75e226ee9b2ec127fa4d619a04065c517
AcadeMeData <michael@acade-me.com>
2021-01-05 09:44:11 +0200

    Aspect ratio & device measurement

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] int timeToWait = 4;
+    int currentSceneIndex;
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0) {
+            StartCoroutine(WaitForTime());
+        }        
+    }
+
+    IEnumerator WaitForTime() {
+        yield return new WaitForSeconds(timeToWait);
+        LoadNextScene();
+       
+    }
+
+    public void LoadNextScene() {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
